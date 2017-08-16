f6a6f8b02e476cec2c7b8c70547812858f9e783e
Christopher deLeon <Christopher.deLeon@Yardi.Com>
2017-08-15 17:15:09 -0700

    add index html

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-var http = require("http");
-http.createServer(function (request, response) {
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   response.end('Welcome to YML');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+var http = require("http"),
+  fs = require('fs');
+  fs.readFile('./index.html', function(err, html){
+    if (err) {
+      throw err;
+    }
+    http.createServer(function (request, response) {
+       // Send the HTTP header
+       // HTTP Status: 200 : OK
+       // Content Type: text/plain
+       response.writeHeader(200, {'Content-Type': 'text/plain'});
+       response.write(html);
+       response.end();
+    }).listen(8081);
+    // Console will print the message
+    console.log('Server running at http://127.0.0.1:8081/');
+  });
