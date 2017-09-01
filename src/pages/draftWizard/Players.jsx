73dcaffd18c735e66f515e94db7e342e424931f6
AmosL <adl.amoslin@gmail.com>
2017-09-01 00:52:33 -0700

    player modal
    added player modal and user selection

--- src/pages/draftWizard/Players.jsx
+++ src/pages/draftWizard/Players.jsx
@@ -1,14 +1,100 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
+
+class SimpleDialog extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            checked: [0],
+        }
+    }
+
+    handleRequestClose = () => {
+        this.props.onRequestClose(this.props.selectedValue);
+    };
+
+    handleListItemClick = value => {
+        this.props.onRequestClose(value);
+    };
+
+    handleToggle = (event, value) => {
+        const { checked } = this.state;
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        this.setState({
+            checked: newChecked,
+        });
+    };
+
+    render() {
+        let open = this.props.open;
+
+        return (
+            <Dialog onRequestClose={this.handleRequestClose} open={open}>
+                <DialogTitle>Select Players</DialogTitle>
+                <div>
+                    <List>
+                        {this.props.users.map(value => (
+                            <ListItem button onClick={event => this.handleToggle(event, value)} key={value.email}>
+                                <Checkbox
+                                    checked={this.state.checked.indexOf(value) !== -1}
+                                    tabIndex="-1"
+                                    disableRipple
+                                />
+                                <ListItemText primary={value.email} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </Dialog>
+        );
+    }
+}
 
 class Players extends Component {
     constructor() {
         super();
+
+        this.state = {
+            users: [],
+            open: false,
+        }
     }
 
+    componentWillMount() {
+        fetch('http://localhost:3001/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                users: json
+            });
+        });
+    }
+
+    handleRequestClose = value => {
+        this.setState({ 
+            open: false 
+        });
+    };
+
     render() {
-        let draft = this.props.draft;
         let pods = this.props.draft.pods;
 
         //Iterate on pods. Create one table per pod.
@@ -33,13 +119,20 @@ class Players extends Component {
                             <TableBody>
                                 <TableRow>
                                     <TableCell>
-                                        <Button raised>Add Player</Button>
+                                        <Button raised onClick={() => this.setState({ open: true })}>Add Player</Button>
                                     </TableCell>
                                 </TableRow>
                             </TableBody>
                         </Table>
                     );
                 })}
+
+                <SimpleDialog
+                    selectedValue={this.state.selectedValue}
+                    open={this.state.open}
+                    onRequestClose={this.handleRequestClose}
+                    users={this.state.users}
+                />
             </section>
         );
     }
